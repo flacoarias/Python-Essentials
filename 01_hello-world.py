f63674361213e968355495708c077cd77fcd98c9
Juan Fernando Arias Benavides <jarias@pucesi.edu.ec>
2023-03-15 18:01:18 -0500

    Primera clase de python

---
+++ 01_hello-world.py
@@ -0,0 +1,6 @@
+print("Hello World!") #Funcion para imprimir
+"""print('Hello World!') #Funcion para imprimir
+print('Hello World!') #Funcion para imprimir
+print('Hello World!') #Funcion para imprimir
+print('Hello World!') #Funcion para imprimir"""
+print("Hello World desde PUCESI!") #Funcion para imprimir
